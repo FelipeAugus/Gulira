e5ff040fd740a04622a11a0120c7933dd72e1a21
Felipe Augusto <augusto.felipao22@gmail.com>
2021-02-16 11:41:22 -0300

    Easteregg
    Adicionado gif após 6 clicks consecutivos dentro de 1s e aprimorado o setTimeout da função punch.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const balls = document.getElementsByClassName("ball");
+const hand = document.getElementsByClassName("hand");
+const mFlip = document.getElementsByClassName("easteregg");
+
+let clear = null;
+
+let flip = 0;
+
 (function(){
-    const balls = document.getElementsByClassName("ball");
-    
     document.onmousemove = function() {
         let x = event.clientX * 100 / window.innerWidth + "%",
             y = event.clientY *100 / window.innerHeight + "%";
@@ -17,12 +23,25 @@ function punch(){
     let x = event.clientX * 100 / window.innerWidth + "%",
         y = event.clientY *100 / window.innerHeight + "%";
 
-    const hand = document.getElementsByClassName("hand");
-
     hand[0].style.left = x;
     hand[0].style.top = y;
     
     hand[0].style.animation = `punch 0.6s ease 0.1s infinite alternate`;
-    setTimeout(function(){hand[0].style.animation = `none`;}, 702);
+    if(clear == null){
+        clear = setTimeout(function(){hand[0].style.animation = `none`;}, 702);
+    }else{
+        clearTimeout(clear);
+        clear = setTimeout(function(){hand[0].style.animation = `none`;}, 702);
+    }
     
+}
+
+function monkeyFlip(){
+    flip++;
+    if (flip == 6){
+        mFlip[0].style.display = `block`;
+        setTimeout(function(){mFlip[0].style.display = `none`;}, 10100);
+    }else{
+        setTimeout(function(){flip = 0;}, 1000);
+    }
 }
