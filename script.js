248714d989660f0fac320fac6ee7a338235421e4
Felipe Augusto <augusto.felipao22@gmail.com>
2021-02-16 10:24:17 -0300

    PunchAtt
    Melhorias no sistema de soco e correção das medidas (estavam todas em vh, passadas para vw as que convinham).

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 (function(){
     const balls = document.getElementsByClassName("ball");
-    const hand = document.getElementsByClassName("hand");
-
+    
     document.onmousemove = function() {
         let x = event.clientX * 100 / window.innerWidth + "%",
             y = event.clientY *100 / window.innerHeight + "%";
-
+    
         for (let index = 0; index <2; index++) {
             balls[index].style.left = x;
             balls[index].style.top = y;
             balls[index].style.transform = `translate(-${x},-${y})`;
         }
-        hand[0].style.left = x;
-        hand[0].style.top = y;
-        hand[0].style.transform = `translate(-${x},-${y})`;
     }
 })();
+
+function punch(){
+    let x = event.clientX * 100 / window.innerWidth + "%",
+        y = event.clientY *100 / window.innerHeight + "%";
+
+    const hand = document.getElementsByClassName("hand");
+
+    hand[0].style.left = x;
+    hand[0].style.top = y;
+    
+    hand[0].style.animation = `punch 0.6s ease 0.1s infinite alternate`;
+    setTimeout(function(){hand[0].style.animation = `none`;}, 702);
+    
+}
